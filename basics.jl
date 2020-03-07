27b292b94c9b7eafd481f0a6260d44abdbd4273c
nikos6ares <61912357+nikos6ares@users.noreply.github.com>
2020-03-07 16:53:15 +0100

    Update and rename exercise1.jl to basics.jl

--- basics.jl
+++ basics.jl
@@ -1,6 +1,3 @@
-using Printf "to use the python command {} {} .float{etc blah}"
-using Plots "to plot"
-
 @time begin   " to calculate the execution time of blah"
 blah
 end
@@ -17,11 +14,11 @@ ones(Int32,L,L) "Creates 1 integers matrix of L X L "
 
 push!(array,item) "Appends item at the end of the array. *Appending is slower than replacing entries* "
 
-@printf("%8.3f \n", 5.12358248582) "the float thing from python"
+
+
 
 findmax(ht) "array made of (maximum_value_of_ht,position)"
 
-savefig(plot(temps,mt),"magnetiz_vs_temp.png") "Just saves the file"
 
 
 
